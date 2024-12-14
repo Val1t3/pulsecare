2a9687457346765a0dc35b5afd13da2474dd14a5
Val1t3 <valentinwoehrel2003@gmail.com>
2024-12-14 22:55:24 +0100

    add: declaration and definition command parsing

--- src/cli/src/CommandParser.cpp
+++ src/cli/src/CommandParser.cpp
@@ -1,5 +1,30 @@
 #include "CommandParser.hpp"
+#include <iostream>
+#include <utility>
 
-cli::CommandParser::CommandParser() {}
+cli::CommandParser::CommandParser() {
+  _commands = std::unordered_map<std::string, command_t>();
+}
 
 cli::CommandParser::~CommandParser() {}
+
+void cli::CommandParser::addCommand(const std::string& command,
+                                    const std::string& description,
+                                    std::function<void()> callback) {
+  _commands[command] = {description, callback};
+}
+
+std::pair<std::string, std::vector<std::string>> cli::CommandParser::parse(
+    const std::string& input) {
+  return std::make_pair("command", std::vector<std::string>());
+}
+
+void cli::CommandParser::execute(const std::string& commandName,
+                                 std::vector<std::string>& args) {}
+
+void cli::CommandParser::help() {
+  for (const auto& command : _commands) {
+    std::cout << command.first << " - " << command.second.description
+              << std::endl;
+  }
+}
--- src/cli/src/CommandParser.hpp
+++ src/cli/src/CommandParser.hpp
@@ -1,11 +1,31 @@
 #ifndef COMMANDPARSER_HPP
 #define COMMANDPARSER_HPP
 
+#include <functional>
+#include <unordered_map>
+#include <utility>
+
 namespace cli {
+
+typedef struct command_s {
+  std::string description;
+  std::function<void()> callback;
+} command_t;
+
 class CommandParser {
  public:
   CommandParser();
   ~CommandParser();
+
+  void addCommand(const std::string& command, const std::string& description,
+                  std::function<void()> callback);
+  std::pair<std::string, std::vector<std::string>> parse(
+      const std::string& input);
+  void execute(const std::string& commandName, std::vector<std::string>& args);
+  void help();
+
+ private:
+  std::unordered_map<std::string, command_t> _commands;
 };
 }  // namespace cli
 
